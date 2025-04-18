5337d60047e6512128d388944077deffba699692
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-18 16:09:59 +0200

    add content script which can extract data

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,8 +1,33 @@
-import { render } from "solid-js/web";
-import { App } from "./components/Demo/app";
+// import { render } from "solid-js/web";
+// import { App } from "./components/Demo/app";
 
-const root = document.createElement("div");
-root.id = "my-extension-root";
-document.body.append(root);
+// const root = document.createElement("div");
+// root.id = "my-extension-root";
+// document.body.append(root);
 
-render(App, root);
+// render(App, root);
+
+const germanTitle = document.querySelector(".episodeGermanTitle")?.textContent;
+const englishTitle = document.querySelector(
+  ".episodeEnglishTitle",
+)?.textContent;
+
+const selectedLanguage = document
+  .querySelector(".selectedLanguage")
+  ?.getAttribute("title"); // can be "Deutsch" (not "German" because current lang is deutsch) or "English" (because current lang is english)
+
+// extract season and episode from url
+// Example: https://s.to/serie/stream/designated-survivor/staffel-1/episode-4
+const url = window.location.href.split("/");
+const season = url.at(-2);
+const episode = url.at(-1);
+const seasonNr = season.split("-").at(1);
+const episodeNr = episode.split("-").at(1);
+
+console.debug(`[M3u8 Finder]: found infos`, {
+  germanTitle,
+  englishTitle,
+  selectedLanguage,
+  seasonNr,
+  episodeNr,
+});
